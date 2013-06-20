c0802bdeaf66871a1b7ac660b477b89bb9c7d0ca
Art Clarke <art.clarke@gmail.com>
2013-06-19 17:58:26 -0700

    wrong dir

--- cross-compile.sh
+++ cross-compile.sh
@@ -6,12 +6,12 @@ HOST=$( ${DIR}/mk/config.guess )
 
 humble_configure()
 {
-  PREFIX=$1
+  PREFIX="$1/$2"
   BUILD=$2
   echo "Building ${DIR}/configure in ${PREFIX}"
   mkdir -p ./$cross_os
   mkdir -p "${PREFIX}"
-  (cd ./"${cross_os}" && (${DIR}/configure --prefix="${PREFIX}" --build="${BUILD}" | tee configure.log) && (make | tee make.out) && make install)
+  (cd ./${cross_os} && (${DIR}/configure --prefix="${PREFIX}" --build="${BUILD}" | tee configure.log) && (make | tee make.out) && make install)
 }
 
 STAGE_DIR="${HUMBLE_HOME:-/tmp}"
