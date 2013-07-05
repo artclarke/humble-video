813921d954b9f682a8ccb096150a3daf37da724c
Art Clarke <art.clarke@gmail.com>
2013-07-04 17:16:40 -0700

    KeyValueBag native tests

---
+++ src/main/gnu/test/io/humble/video/KeyValueBagTest.cpp
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Art Clarke.  All rights reserved.
+ *  
+ * This file is part of Humble-Video.
+ *
+ * Humble-Video is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Humble-Video is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Humble-Video.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
+#include <cstring>
+#include "KeyValueBagTest.h"
+
+#include <io/humble/ferry/Logger.h>
+#include <io/humble/ferry/RefPointer.h>
+#include <io/humble/video/KeyValueBag.h>
+#include <io/humble/video/Source.h>
+
+using namespace io::humble::ferry;
+using namespace io::humble::video;
+
+VS_LOG_SETUP(VS_CPP_PACKAGE);
+
+KeyValueBagTest :: KeyValueBagTest()
+{
+  mSampleFile[0] =0;
+  char *fixtureDirectory = getenv("VS_TEST_FIXTUREDIR");
+  if (!fixtureDirectory) {
+    TSM_ASSERT("no fixture dir", false);
+    throw new std::runtime_error("Must define environment variable VS_TEST_FIXTUREDIR");
+  }
+  snprintf(mSampleFile, sizeof(mSampleFile), "%s/%s", fixtureDirectory, "testfile.mp3");
+  VS_LOG_TRACE("Sample File: %s", mSampleFile);
+}
+
+KeyValueBagTest :: ~KeyValueBagTest()
+{
+}
+
+void
+KeyValueBagTest :: setUp()
+{
+
+}
+
+void
+KeyValueBagTest :: tearDown()
+{
+
+}
+
+void
+KeyValueBagTest :: testCreation()
+{
+  RefPointer<KeyValueBag> metaData = KeyValueBag::make();
+  TSM_ASSERT_EQUALS("should be zero", metaData->getNumKeys(), 0);
+  metaData->setValue("foo", "bar");
+  TSM_ASSERT_EQUALS("should be zero", metaData->getNumKeys(), 1);
+  const char* value = metaData->getValue("foo", KeyValueBag::KVB_NONE);
+  TSM_ASSERT("should be bar", strcmp("bar", value)==0);
+  TSM_ASSERT("should be empty", !metaData->getValue("none",
+        KeyValueBag::KVB_NONE));
+}
+
+void
+KeyValueBagTest :: testSourceGetMetaData()
+{
+  RefPointer<Source> source=Source::make();
+  int32_t retval = source->open(mSampleFile, 0, false, true, 0, 0);
+  TS_ASSERT(retval >= 0);
+
+  RefPointer<KeyValueBag> meta = source->getMetaData();
+  TSM_ASSERT("got meta data", meta);
+  int32_t numKeys = meta->getNumKeys();
+  TSM_ASSERT("should be right", numKeys >= 5);
+  TSM_ASSERT("should be right", numKeys <= 7);
+  for(int32_t i = 0; i < numKeys; i++)
+  {
+    const char* key = meta->getKey(i);
+    TSM_ASSERT("should be found", key);
+    TSM_ASSERT("should be found", *key);
+    const char* value = meta->getValue(key, KeyValueBag::KVB_NONE);
+    TSM_ASSERT("should be found", value);
+    TSM_ASSERT("should be found", *value);
+  }
+  retval = source->close();
+  TS_ASSERT(retval >= 0);
+}
+
---
+++ src/main/gnu/test/io/humble/video/KeyValueBagTest.h
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Art Clarke.  All rights reserved.
+ *  
+ * This file is part of Humble-Video.
+ *
+ * Humble-Video is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Humble-Video is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Humble-Video.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
+#ifndef KEYVALUEBAGTEST_H_
+#define KEYVALUEBAGTEST_H_
+
+#include <io/humble/testutils/TestUtils.h>
+
+class KeyValueBagTest: public CxxTest::TestSuite
+{
+public:
+  KeyValueBagTest();
+  virtual
+  ~KeyValueBagTest();
+  void setUp();
+  void tearDown();
+  void testCreation();
+  void testSourceGetMetaData();
+
+private:
+  char mSampleFile[2048];
+};
+
+#endif /* KEYVALUEBAGTEST_H_ */
