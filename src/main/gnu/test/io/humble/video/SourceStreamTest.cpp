8f8d8388933faab47d5bde122b883ba2fe3a85e6
Art Clarke <art.clarke@gmail.com>
2013-07-07 10:59:42 -0700

    basic test and passes valgrind

--- src/main/gnu/test/io/humble/video/SourceStreamTest.cpp
+++ src/main/gnu/test/io/humble/video/SourceStreamTest.cpp
@@ -1,17 +1,18 @@
 #include <io/humble/ferry/RefPointer.h>
 #include <io/humble/ferry/Logger.h>
-#include <io/humble/xuggler/IStream.h>
-#include <io/humble/xuggler/IStreamCoder.h>
-#include <io/humble/xuggler/ICodec.h>
-#include <io/humble/xuggler/Global.h>
+#include <io/humble/video/Source.h>
+#include <io/humble/video/SourceStream.h>
+#include <io/humble/video/Global.h>
 #include "SourceStreamTest.h"
 
-using namespace VS_CPP_NAMESPACE;
+using namespace io::humble::ferry;
+using namespace io::humble::video;
 
 VS_LOG_SETUP(VS_CPP_PACKAGE);
 
 SourceStreamTest::SourceStreamTest()
 {
+  VS_LOG_TRACE("Creation!");
 }
 
 SourceStreamTest::~SourceStreamTest()
@@ -31,59 +32,31 @@ SourceStreamTest::tearDown()
 void
 SourceStreamTest::testCreationAndDestruction()
 {
-  RefPointer<IStream> stream;
-  RefPointer<IStreamCoder> coder;
-  RefPointer<ICodec> codec;
-  RefPointer<IRational> rational;
+  RefPointer<Source> s = Source::make();
+  TestData::Fixture* f = mFixtures.getFixture(0);
+  char path[2048];
+  mFixtures.fillPath(f, path, sizeof(path));
 
-  h->setupReading(h->SAMPLE_FILE);
+  int32_t retval;
+  retval = s->open(path, 0, false, true, 0, 0);
+  TS_ASSERT(retval >= 0);
 
-  for (int i = 0; i < h->num_streams; i++)
-  {
-    VS_LOG_TRACE("Starting test of stream: %d", i);
+  int32_t ns = s->getNumStreams();
+  TS_ASSERT_EQUALS(ns, f->num_streams);
 
-    VS_LOG_TRACE("Stream test: Getting stream: %d", i);
-    stream = h->streams[i];
-    TSM_ASSERT("could not get stream", stream);
+  // Remember this for after the close -- we best not crash
+  RefPointer<SourceStream> firstStream = s->getSourceStream(0);
 
-    // container, helper, and me.
-    TSM_ASSERT_EQUALS("stream", stream->getCurrentRefCount(), 3);
+  for(int i = 0; i < ns; i++) {
+    RefPointer<SourceStream> ss = s->getSourceStream(i);
+    TS_ASSERT(ss);
 
-    TSM_ASSERT("stream index did not match",
-        stream->getIndex() == i);
-
-    VS_LOG_TRACE("Stream test: Getting coder: %d", i);
-    coder = stream->getStreamCoder();
-    TSM_ASSERT("could not get coder for stream", coder);
-
-    TSM_ASSERT_EQUALS("coder", coder->getCurrentRefCount(), 3);
-
-    VS_LOG_TRACE("Stream test: Getting codec: %d", i);
-    codec = coder->getCodec();
-    TSM_ASSERT("could not get codec", codec);
-
-    TSM_ASSERT_EQUALS("codec", codec->getCurrentRefCount(), 3);
-
-    if (!codec->getType() == ICodec::CODEC_TYPE_AUDIO &&
-        !codec->getType() == ICodec::CODEC_TYPE_VIDEO)
-    {
-      VS_LOG_ERROR("Unexpected type of codec");
-      TS_ASSERT(false);
-    }
-
-    if (codec->getType() == ICodec::CODEC_TYPE_VIDEO)
-    {
-      rational = stream->getFrameRate();
-      if (h->expected_frame_rate > 0)
-        TSM_ASSERT_DELTA("unexpected frame rate",
-            rational->getDouble(),
-            h->expected_frame_rate, 0.0001);
-      rational = stream->getTimeBase();
-      if (h->expected_time_base > 0)
-        TSM_ASSERT_DELTA("unexpected frame rate",
-            rational->getDouble(),
-            h->expected_time_base, 0.0001);
-      VS_LOG_TRACE("Finished test of stream: %d", i);
-    }
+    // now, let's go a testing.
+    TS_ASSERT_EQUALS(i, ss->getIndex());
   }
+
+  retval = s->close();
+  TS_ASSERT(retval >= 0);
+
+  TS_ASSERT(!firstStream->getContainer());
 }
--- src/main/gnu/test/io/humble/video/SourceStreamTest.h
+++ src/main/gnu/test/io/humble/video/SourceStreamTest.h
@@ -2,19 +2,22 @@
 #define __SOURCESTREAM_TEST_H__
 
 #include <io/humble/testutils/TestUtils.h>
+#include "TestData.h"
 using namespace VS_CPP_NAMESPACE;
 
-class StreamTest : public CxxTest::TestSuite
+class SourceStreamTest : public CxxTest::TestSuite
 {
-  public:
-    StreamTest();
-    virtual ~StreamTest();
-    void setUp();
-    void tearDown();
-    void testCreationAndDestruction();
-  private:
+public:
+  SourceStreamTest();
+  virtual
+  ~SourceStreamTest();
+  void
+  setUp();
+  void
+  tearDown();
+  void testCreationAndDestruction();
+private:
+  TestData mFixtures;
 };
 
-
 #endif // __SOURCESTREAM_TEST_H__
-
