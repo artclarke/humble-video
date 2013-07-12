c94145c213f89ae1abb851204a313ea8080a794c
Art Clarke <art.clarke@gmail.com>
2013-07-12 15:17:03 -0700

    empty MediaAudioTester shell

---
+++ src/main/gnu/test/io/humble/video/MediaAudioTest.cpp
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Art Clarke.  All rights reserved.
+ *  
+ * This file is part of Humble-Video.
+ *
+ * Humble-Video is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Humble-Video is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Humble-Video.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+/*
+ * MediaAudioTest.cpp
+ *
+ *  Created on: Jul 12, 2013
+ *      Author: aclarke
+ */
+
+#include "MediaAudioTest.h"
+#include <io/humble/ferry/RefPointer.h>
+
+namespace io {
+namespace humble {
+namespace video {
+
+MediaAudioTest::MediaAudioTest() {
+}
+
+MediaAudioTest::~MediaAudioTest() {
+}
+
+void
+MediaAudioTest::testCreation() {
+}
+
+void
+MediaAudioTest::testCreationFromBuffer() {
+}
+
+} /* namespace video */
+} /* namespace humble */
+} /* namespace io */
