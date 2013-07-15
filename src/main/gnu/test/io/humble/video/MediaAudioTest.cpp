6ac1100a48ba39445fd5ff59c6db8704d16e9161
Art Clarke <art.clarke@gmail.com>
2013-07-14 19:41:23 -0700

    add tests that check for exceptions

--- src/main/gnu/test/io/humble/video/MediaAudioTest.cpp
+++ src/main/gnu/test/io/humble/video/MediaAudioTest.cpp
@@ -26,6 +26,7 @@
 #include "MediaAudioTest.h"
 #include <io/humble/ferry/RefPointer.h>
 #include <io/humble/ferry/LoggerStack.h>
+#include <io/humble/ferry/HumbleException.h>
 
 using namespace io::humble::video;
 using namespace io::humble::ferry;
@@ -51,42 +52,40 @@ MediaAudioTest::testCreation() {
     LoggerStack stack;
     stack.setGlobalLevel(Logger::LEVEL_ERROR, false);
 
-    audio = MediaAudio::make(-1, sampleRate, channels, layout, format);
-    TS_ASSERT(!audio);
+    TS_ASSERT_THROWS(MediaAudio::make(-1, sampleRate, channels, layout, format),
+        HumbleInvalidArgument);
   }
 
   {
     LoggerStack stack;
     stack.setGlobalLevel(Logger::LEVEL_ERROR, false);
 
-    audio = MediaAudio::make(numSamples, -1, channels, layout, format);
-    TS_ASSERT(!audio);
+    TS_ASSERT_THROWS(MediaAudio::make(numSamples, -1, channels, layout, format),
+        HumbleInvalidArgument);
   }
 
   {
     LoggerStack stack;
     stack.setGlobalLevel(Logger::LEVEL_ERROR, false);
 
-    audio = MediaAudio::make(numSamples, sampleRate, -1, layout, format);
-    TS_ASSERT(!audio);
+    TS_ASSERT_THROWS(MediaAudio::make(numSamples, sampleRate, -1, layout, format),
+        HumbleInvalidArgument);
   }
 
   {
     LoggerStack stack;
     stack.setGlobalLevel(Logger::LEVEL_ERROR, false);
 
-    audio = MediaAudio::make(numSamples, sampleRate, channels + 1, layout,
-        format);
-    TS_ASSERT(!audio);
+    TS_ASSERT_THROWS(MediaAudio::make(numSamples, sampleRate, channels + 1, layout,
+        format), HumbleInvalidArgument);
   }
 
   {
     LoggerStack stack;
     stack.setGlobalLevel(Logger::LEVEL_ERROR, false);
 
-    audio = MediaAudio::make(numSamples, sampleRate, channels, layout,
-        AudioFormat::SAMPLE_FMT_NONE);
-    TS_ASSERT(!audio);
+    TS_ASSERT_THROWS(MediaAudio::make(numSamples, sampleRate, channels, layout,
+        AudioFormat::SAMPLE_FMT_NONE), HumbleInvalidArgument);
   }
 
   // And this should be valid
@@ -113,11 +112,14 @@ MediaAudioTest::testCreation() {
     LoggerStack stack;
     stack.setGlobalLevel(Logger::LEVEL_ERROR, false);
 
-    TS_ASSERT(audio->setComplete(audio->getMaxNumSamples()+1, Global::NO_PTS) < 0);
-    TS_ASSERT(audio->setComplete(-1, Global::NO_PTS) < 0);
-    TS_ASSERT(audio->setComplete(0, Global::NO_PTS) < 0);
+    TS_ASSERT_THROWS(
+        audio->setComplete(audio->getMaxNumSamples() + 1, Global::NO_PTS),
+        HumbleInvalidArgument);
+    TS_ASSERT_THROWS(audio->setComplete(-1, Global::NO_PTS), HumbleInvalidArgument);
+    TS_ASSERT_THROWS(audio->setComplete(0, Global::NO_PTS), HumbleInvalidArgument);
   }
-  TS_ASSERT(audio->setComplete(audio->getMaxNumSamples()-1, Global::NO_PTS) >= 0);
+
+  audio->setComplete(audio->getMaxNumSamples() - 1, Global::NO_PTS);
   TS_ASSERT(audio->isComplete());
 
   for (int i = 0; i < channels; i++) {
@@ -139,8 +141,7 @@ MediaAudioTest::testCreation() {
   for (int i = 1; i < channels; i++) {
     LoggerStack stack;
     stack.setGlobalLevel(Logger::LEVEL_ERROR, false);
-    buf = audio->getData(i);
-    TS_ASSERT(!buf);
+    TS_ASSERT_THROWS(audio->getData(i), HumbleInvalidArgument);
   }
 
 }
@@ -153,13 +154,13 @@ MediaAudioTest::testCreationFromBufferPacked() {
   const AudioChannel::Layout layout = AudioChannel::CH_LAYOUT_7POINT1;
   const AudioFormat::Type format = AudioFormat::SAMPLE_FMT_DBL;
 
-  int32_t bufSize = AudioFormat::getBufferSizeNeeded(numSamples, channels, format);
+  int32_t bufSize = AudioFormat::getBufferSizeNeeded(numSamples, channels,
+      format);
   RefPointer<IBuffer> src = IBuffer::make(0, bufSize);
-  double* srcData = (double*)src->getBytes(0, bufSize);
+  double* srcData = (double*) src->getBytes(0, bufSize);
 
   // now, let's go nuts!
-  for(size_t i = 0; i < bufSize/sizeof(double); i++)
-  {
+  for (size_t i = 0; i < bufSize / sizeof(double); i++) {
     srcData[i] = i;
   }
 
@@ -169,18 +170,17 @@ MediaAudioTest::testCreationFromBufferPacked() {
     LoggerStack stack;
     stack.setGlobalLevel(Logger::LEVEL_ERROR, false);
 
-    audio = MediaAudio::make(0, numSamples, sampleRate, channels, layout,
-        format);
-    TS_ASSERT(!audio);
+    TS_ASSERT_THROWS(MediaAudio::make(0, numSamples, sampleRate, channels, layout,
+        format), HumbleInvalidArgument);
   }
-  audio = MediaAudio::make(src.value(), numSamples, sampleRate, channels, layout,
-      format);
+  audio = MediaAudio::make(src.value(), numSamples, sampleRate, channels,
+      layout, format);
   TS_ASSERT(audio);
 
   TS_ASSERT_EQUALS(1, audio->getNumDataPlanes());
   RefPointer<IBuffer> dst = audio->getData(0);
   TS_ASSERT(dst);
-  double* dstData = (double*)dst->getBytes(0, dst->getBufferSize());
+  double* dstData = (double*) dst->getBytes(0, dst->getBufferSize());
 
   // the test in packed data is simple -- the buffers should be equal!
   TS_ASSERT_EQUALS(src->getBufferSize(), dst->getBufferSize());
@@ -196,17 +196,18 @@ MediaAudioTest::testCreationFromBufferPlanar() {
   const AudioChannel::Layout layout = AudioChannel::CH_LAYOUT_UNKNOWN;
   const AudioFormat::Type format = AudioFormat::SAMPLE_FMT_DBLP;
 
-  int32_t bufSize = AudioFormat::getBufferSizeNeeded(numSamples, channels, format);
+  int32_t bufSize = AudioFormat::getBufferSizeNeeded(numSamples, channels,
+      format);
   // test that there is rounding up
-  int32_t minSize = AudioFormat::getBytesPerSample(format)*numSamples*channels;
+  int32_t minSize = AudioFormat::getBytesPerSample(format) * numSamples
+      * channels;
   TS_ASSERT_LESS_THAN(minSize, bufSize);
 
   RefPointer<IBuffer> src = IBuffer::make(0, bufSize);
-  double* srcData = (double*)src->getBytes(0, bufSize);
+  double* srcData = (double*) src->getBytes(0, bufSize);
 
   // now, let's go nuts!
-  for(size_t i = 0; i < bufSize/sizeof(double); i++)
-  {
+  for (size_t i = 0; i < bufSize / sizeof(double); i++) {
     srcData[i] = i;
   }
 
@@ -216,64 +217,63 @@ MediaAudioTest::testCreationFromBufferPlanar() {
     LoggerStack stack;
     stack.setGlobalLevel(Logger::LEVEL_ERROR, false);
 
-    audio = MediaAudio::make(0, numSamples, sampleRate, channels, layout,
-        format);
-    TS_ASSERT(!audio);
+    TS_ASSERT_THROWS(MediaAudio::make(0, numSamples, sampleRate, channels, layout,
+        format), HumbleInvalidArgument);
   }
-  audio = MediaAudio::make(src.value(), numSamples, sampleRate, channels, layout,
-      format);
+  audio = MediaAudio::make(src.value(), numSamples, sampleRate, channels,
+      layout, format);
   TS_ASSERT(audio);
 
   TS_ASSERT_EQUALS(channels, audio->getNumDataPlanes());
 
   // sigh; time to test each plane.
-  for(int i = 0; i < channels; i++) {
+  for (int i = 0; i < channels; i++) {
     RefPointer<IBuffer> dst = audio->getData(i);
     TS_ASSERT(dst);
-    double* dstData = (double*)dst->getBytes(0, dst->getBufferSize());
+    double* dstData = (double*) dst->getBytes(0, dst->getBufferSize());
 
     // the values should be monotonically increasing given how we set them.
     double last = dstData[0];
-    for(size_t j=1; j < dst->getBufferSize()/sizeof(double); j++)
-    {
-      TS_ASSERT_DELTA(last+1, dstData[j], 0.001);
+    for (size_t j = 1; j < dst->getBufferSize() / sizeof(double); j++) {
+      TS_ASSERT_DELTA(last + 1, dstData[j], 0.001);
       last = dstData[j];
     }
   }
 }
 
 void
-MediaAudioTest::testCopy()
-{
+MediaAudioTest::testCopy() {
   const int32_t numSamples = 155; // I choose an odd number because HV will align up to 32
   const int32_t sampleRate = 22050;
   const int32_t channels = 15; // choose a large # of channels to make sure we expand the Frame
   const AudioChannel::Layout layout = AudioChannel::CH_LAYOUT_UNKNOWN;
   const AudioFormat::Type format = AudioFormat::SAMPLE_FMT_DBLP;
 
-  int32_t bufSize = AudioFormat::getBufferSizeNeeded(numSamples, channels, format);
+  int32_t bufSize = AudioFormat::getBufferSizeNeeded(numSamples, channels,
+      format);
   // test that there is rounding up
-  int32_t minSize = AudioFormat::getBytesPerSample(format)*numSamples*channels;
+  int32_t minSize = AudioFormat::getBytesPerSample(format) * numSamples
+      * channels;
   TS_ASSERT_LESS_THAN(minSize, bufSize);
 
   RefPointer<IBuffer> src = IBuffer::make(0, bufSize);
-  double* srcData = (double*)src->getBytes(0, bufSize);
+  double* srcData = (double*) src->getBytes(0, bufSize);
 
   // now, let's go nuts!
-  for(size_t i = 0; i < bufSize/sizeof(double); i++)
-  {
+  for (size_t i = 0; i < bufSize / sizeof(double); i++) {
     srcData[i] = i;
   }
 
   RefPointer<MediaAudio> audio;
-  audio = MediaAudio::make(src.value(), numSamples, sampleRate, channels, layout,
-      format);
+  audio = MediaAudio::make(src.value(), numSamples, sampleRate, channels,
+      layout, format);
   TS_ASSERT(audio);
 
   TS_ASSERT_EQUALS(channels, audio->getNumDataPlanes());
 
-  bool tests[] = {true, false};
-  for(size_t i = 0; i < sizeof(tests)/sizeof(*tests); i++) {
+  bool tests[] =
+    { true, false };
+  for (size_t i = 0; i < sizeof(tests) / sizeof(*tests); i++) {
 
     // now let's make a copy
     RefPointer<MediaAudio> copy = MediaAudio::make(audio.value(), tests[i]);
@@ -286,24 +286,24 @@ MediaAudioTest::testCopy()
     TS_ASSERT_EQUALS(copy->getSampleRate(), audio->getSampleRate());
     TS_ASSERT_EQUALS(copy->getFormat(), audio->getFormat());
 
-      for(int32_t j = 0; j < audio->getNumDataPlanes(); j++) {
-        RefPointer<IBuffer> srcBuf = audio->getData(j);
-        RefPointer<IBuffer> dstBuf = copy->getData(j);
+    for (int32_t j = 0; j < audio->getNumDataPlanes(); j++) {
+      RefPointer<IBuffer> srcBuf = audio->getData(j);
+      RefPointer<IBuffer> dstBuf = copy->getData(j);
 
-        int32_t planeSize = srcBuf->getBufferSize();
-        TS_ASSERT_EQUALS(planeSize, dstBuf->getBufferSize());
+      int32_t planeSize = srcBuf->getBufferSize();
+      TS_ASSERT_EQUALS(planeSize, dstBuf->getBufferSize());
 
-        uint8_t* srcBytes = (uint8_t*)srcBuf->getBytes(0, planeSize);
-        uint8_t* dstBytes = (uint8_t*)dstBuf->getBytes(0, planeSize);
-        if (tests[i]) {
-          for(int32_t k = 0; k < planeSize; k++) {
-            // should be byte-by-byte the same
-            TS_ASSERT_EQUALS(srcBytes[k], dstBytes[k]);
-          }
-        } else {
-          TS_ASSERT_EQUALS(srcBytes, dstBytes);
+      uint8_t* srcBytes = (uint8_t*) srcBuf->getBytes(0, planeSize);
+      uint8_t* dstBytes = (uint8_t*) dstBuf->getBytes(0, planeSize);
+      if (tests[i]) {
+        for (int32_t k = 0; k < planeSize; k++) {
+          // should be byte-by-byte the same
+          TS_ASSERT_EQUALS(srcBytes[k], dstBytes[k]);
         }
+      } else {
+        TS_ASSERT_EQUALS(srcBytes, dstBytes);
       }
+    }
   }
 
 }
