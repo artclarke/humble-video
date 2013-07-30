4ddd2464265c3c6ced74df3f756cba173b2386be
Art Clarke <art.clarke@gmail.com>
2013-07-30 11:52:40 -0700

    shell for tests

---
+++ src/main/gnu/test/io/humble/video/MediaPictureResamplerTest.cpp
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Art Clarke.  All rights reserved.
+ *  
+ * This file is part of Humble-Video.
+ *
+ * Humble-Video is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Humble-Video is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Humble-Video.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+/*
+ * MediaPictureResamplerTest.cpp
+ *
+ *  Created on: Jul 30, 2013
+ *      Author: aclarke
+ */
+
+#include "MediaPictureResamplerTest.h"
+#include <io/humble/ferry/RefPointer.h>
+#include <io/humble/video/MediaPictureResampler.h>
+
+using namespace io::humble::ferry;
+using namespace io::humble::video;
+
+MediaPictureResamplerTest::MediaPictureResamplerTest() {
+}
+
+MediaPictureResamplerTest::~MediaPictureResamplerTest() {
+}
+
+void
+MediaPictureResamplerTest::testCreation() {
+  TS_SKIP("Not implemented");
+}
---
+++ src/main/gnu/test/io/humble/video/MediaPictureResamplerTest.h
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Art Clarke.  All rights reserved.
+ *  
+ * This file is part of Humble-Video.
+ *
+ * Humble-Video is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Humble-Video is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Humble-Video.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+/*
+ * MediaPictureResamplerTest.h
+ *
+ *  Created on: Jul 30, 2013
+ *      Author: aclarke
+ */
+
+#ifndef MEDIAPICTURERESAMPLERTEST_H_
+#define MEDIAPICTURERESAMPLERTEST_H_
+
+#include <io/humble/testutils/TestUtils.h>
+
+class MediaPictureResamplerTest : public CxxTest::TestSuite
+{
+public:
+  MediaPictureResamplerTest();
+  virtual
+  ~MediaPictureResamplerTest();
+  void testCreation();
+};
+
+#endif /* MEDIAPICTURERESAMPLERTEST_H_ */
