ee23120b291ddf93a0ec331545788132b9598cfd
Art Clarke <art.clarke@gmail.com>
2013-07-31 07:50:08 -0700

    test creation

--- src/main/gnu/test/io/humble/video/MediaAudioResamplerTest.cpp
+++ src/main/gnu/test/io/humble/video/MediaAudioResamplerTest.cpp
@@ -24,6 +24,15 @@
  */
 
 #include "MediaAudioResamplerTest.h"
+#include <io/humble/ferry/LoggerStack.h>
+#include <io/humble/ferry/RefPointer.h>
+#include <io/humble/ferry/HumbleException.h>
+#include <io/humble/video/MediaAudioResampler.h>
+#include <io/humble/video/MediaAudio.h>
+
+
+using namespace io::humble::ferry;
+using namespace io::humble::video;
 
 MediaAudioResamplerTest::MediaAudioResamplerTest() {
 }
@@ -34,5 +43,44 @@ MediaAudioResamplerTest::~MediaAudioResamplerTest() {
 void
 MediaAudioResamplerTest::testCreation()
 {
-  TS_SKIP("Not implemented");
+  RefPointer<MediaAudioResampler> resampler;
+  AudioChannel::Layout outLayout = AudioChannel::CH_LAYOUT_STEREO;
+  AudioChannel::Layout inLayout = AudioChannel::CH_LAYOUT_5POINT1;
+  int32_t outSampleRate = 22050;
+  int32_t inSampleRate = 44100;
+  AudioFormat::Type outFormat = AudioFormat::SAMPLE_FMT_S16;
+  AudioFormat::Type inFormat = AudioFormat::SAMPLE_FMT_DBLP;
+
+  {
+    LoggerStack stack;
+    stack.setGlobalLevel(Logger::LEVEL_ERROR, false);
+
+    TS_ASSERT_THROWS(
+        MediaAudioResampler::make(AudioChannel::CH_LAYOUT_UNKNOWN, outSampleRate, outFormat,
+            inLayout, inSampleRate, inFormat),
+            HumbleInvalidArgument);
+    TS_ASSERT_THROWS(
+        MediaAudioResampler::make(outLayout, 0, outFormat,
+            inLayout, inSampleRate, inFormat),
+            HumbleInvalidArgument);
+    TS_ASSERT_THROWS(
+        MediaAudioResampler::make(outLayout, outSampleRate, AudioFormat::SAMPLE_FMT_NONE,
+            inLayout, inSampleRate, inFormat),
+            HumbleInvalidArgument);
+    TS_ASSERT_THROWS(
+        MediaAudioResampler::make(outLayout, outSampleRate, outFormat,
+            AudioChannel::CH_LAYOUT_UNKNOWN, inSampleRate, inFormat),
+            HumbleInvalidArgument);
+    TS_ASSERT_THROWS(
+        MediaAudioResampler::make(outLayout, outSampleRate, outFormat,
+            inLayout, 0, inFormat),
+            HumbleInvalidArgument);
+    TS_ASSERT_THROWS(
+        MediaAudioResampler::make(outLayout, outSampleRate, outFormat,
+            inLayout, inSampleRate, AudioFormat::SAMPLE_FMT_NONE),
+            HumbleInvalidArgument);
+  }
+  resampler = MediaAudioResampler::make(outLayout, outSampleRate, outFormat,
+      inLayout, inSampleRate, inFormat);
+  TS_ASSERT(resampler);
 }
