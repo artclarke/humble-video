014655bec71454f4caeacb383ada75f16a866a82
Art Clarke <art.clarke@gmail.com>
2013-08-10 11:40:04 -0700

    a test cookbook

---
+++ humble-video-chef/cookbooks/mingw_compiler/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: mingw_compiler
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "gcc-mingw32" do
+  action :install
+end
