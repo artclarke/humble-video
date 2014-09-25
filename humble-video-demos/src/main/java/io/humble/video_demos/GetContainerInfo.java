fa8468a6d5974c05d83b5f34178d63b4e5005e14
Art Clarke <art.clarke@gmail.com>
2014-09-25 08:00:56 -0700

    Clean up code, add comments, and basically make it a worth demo.

--- humble-video-demos/src/main/java/io/humble/video_demos/GetContainerInfo.java
+++ humble-video-demos/src/main/java/io/humble/video_demos/GetContainerInfo.java
@@ -20,7 +20,6 @@ package io.humble.video_demos;
 
 import java.io.IOException;
 
-import io.humble.video.Codec;
 import io.humble.video.Decoder;
 import io.humble.video.Demuxer;
 import io.humble.video.DemuxerFormat;
@@ -87,43 +86,66 @@ public class GetContainerInfo {
    * @throws InterruptedException if process is interrupted while querying.
    */
   private static void getInfo(String arg, Options options) throws InterruptedException, IOException {
-    final Demuxer container = Demuxer.make();
-    // attempt to open the container. Notes that we assume no extra streams
-    // can be added, and that we should attempt to query StreamMetaData (false and true
-    // respectively).
-    container.open(arg, null, false, true, null, null);
-    final DemuxerFormat format = container.getFormat();
-    System.out.printf("URL: '%s' (%s: %s)\n", container.getURL(), format.getLongName(), format.getName());
+    // In Humble, all objects have special contructors named 'make'.
+    
+    // A Demuxer opens up media containers, parses  and de-multiplexes the streams
+    // of media data without those containers.
+    final Demuxer demuxer = Demuxer.make();
 
-    // get metadata
-    KeyValueBag metadata = container.getMetaData();
+    // We open the demuxer by pointing it at a URL.
+    demuxer.open(arg, null, false, true, null, null);
+    
+    // Once we've opened a demuxer, Humble can make a guess about the
+    // DemuxerFormat. Humble supports over 100+ media container formats.
+    final DemuxerFormat format = demuxer.getFormat();
+    System.out.printf("URL: '%s' (%s: %s)\n", demuxer.getURL(), format.getLongName(), format.getName());
+
+    // Many programs that make containers, such as iMovie or Adobe Elements, will
+    // insert meta-data about the container. Here we extract that meta data and print it.
+    KeyValueBag metadata = demuxer.getMetaData();
     System.out.println("MetaData:");
     for(String key: metadata.getKeys())
       System.out.printf("  %s: %s\n", key, metadata.getValue(key));
 
-    // print out some basics about the container
-    final String formattedDuration = formatTimeStamp(container.getDuration());
+    // There are a few other key pieces of information that are interesting for
+    // most containers; The duration, the starting time, and the estimated bit-rate.
+    // This code extracts all three.
+    final String formattedDuration = formatTimeStamp(demuxer.getDuration());
     System.out.printf("Duration: %s, start: %f, bitrate: %d kb/s\n",
         formattedDuration,
-        container.getStartTime() == Global.NO_PTS ? 0 : container.getStartTime() / 1000000.0,
-            container.getBitRate()/1000);
+        demuxer.getStartTime() == Global.NO_PTS ? 0 : demuxer.getStartTime() / 1000000.0,
+            demuxer.getBitRate()/1000);
     
-    // now, let's print out details on each stream.
-    int ns = container.getNumStreams();
+    // Finally, a container consists of several different independent streams of
+    // data called Streams. In Humble there are two objects that represent streams:
+    // DemuxerStream (when you are reading) and MuxerStreams (when you are writing).
+    
+    // First find the number of streams in this container.
+    int ns = demuxer.getNumStreams();
+    
+    // Now, let's iterate through each of them.
     for (int i = 0; i < ns; i++) {
-      DemuxerStream stream = container.getStream(i);
-      Decoder d = stream.getDecoder();
-      System.out.printf(" Stream #0.%1$d (%s): %s\n", i, d != null ? d.toString() : "unknown coder");
-      System.out.println("  Metadata:");
+      DemuxerStream stream = demuxer.getStream(i);
+
       metadata = stream.getMetaData();
+      // Language is usually embedded as metadata in a stream.
+      final String language = metadata.getValue("language");
+      
+      // We will only be able to make a decoder for streams we can actually
+      // decode, so the caller should check for null.
+      Decoder d = stream.getDecoder();
+
+      System.out.printf(" Stream #0.%1$d (%2$s): %3$s\n", i, language, d != null ? d.toString() : "unknown coder");
+      System.out.println("  Metadata:");
       for(String key: metadata.getKeys())
-        System.out.printf("  %s: %s\n", key, metadata.getValue(key));
+        System.out.printf("    %s: %s\n", key, metadata.getValue(key));
     }
   }
 
   /**
-   * @param duration
-   * @return
+   * Pretty prints a timestamp (in {@link Global.NO_PTS} units) into a string.
+   * @param duration A timestamp in {@link Global.NO_PTS} units).
+   * @return A string representing the duration.
    */
   private static String formatTimeStamp(long duration) {
     if (duration == Global.NO_PTS) {
