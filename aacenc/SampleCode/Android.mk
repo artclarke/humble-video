c83b71c52653eb3e0f93feb4e22ccde9952f74c2
Art Clarke <art.clarke@gmail.com>
2018-11-17 12:06:28 -0800

    Squashed 'humble-video-captive/src/main/gnu/libvo-aacenc/csrc/' content from commit 5e81884844f
    git-subtree-dir: humble-video-captive/src/main/gnu/libvo-aacenc/csrc
    git-subtree-split: 5e81884844fa025671d9dba2fc9f6cda8e9b15e1

---
+++ aacenc/SampleCode/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    AAC_E_SAMPLES.c \
+    ../../common/cmnMemory.c
+
+LOCAL_MODULE_TAGS := debug
+
+LOCAL_MODULE := AACEncTest
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_SHARED_LIBRARIES := \
+    libstagefright \
+    libdl
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/ \
+    $(LOCAL_PATH)/../../common \
+    $(LOCAL_PATH)/../../common/include \
+
+include $(BUILD_EXECUTABLE)
